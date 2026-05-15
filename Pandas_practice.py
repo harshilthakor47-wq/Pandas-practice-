7d99c647f6fd4f76dc3542297a50d263dfdd9bbf
Harshil Thakor <282322850+harshilthakor47-wq@users.noreply.github.com>
2026-05-15 20:06:37 +0530

    Create Pandas_practice.py

---
+++ Pandas_practice.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+data = {
+
+        "name" : ["A","B","C","D"],
+
+        "math": [45,60,88,30],
+
+        "science": [78,55,92,40]
+        
+}
+
+df=pd.DataFrame(data)
+
+
+
+print(df[["math","science"]])
+
+
+
+
+print(df.loc[0])
+
+
+
+
+df["total"]=df["math"]+df["science"]
+
+print(df["total"])
+
+print(df)
+
+
+print(df[df["math"]>50])
+
+
+print(df["math"])
+print(df["math"].mean())
+
+
+df["average"] = (df["math"]+df["science"])/2
+
+
+print(df)
+
+print(df[df["average"]>60])
+
+print(df)
+print(df["total"].idxmax())
